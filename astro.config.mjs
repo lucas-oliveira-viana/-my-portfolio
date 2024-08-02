4c45b2a15cdfeb7911803e0578d1d5e65e2d28d6
lucas-oliveira-viana <lucas_oliveira_viana@hotmail.com>
2024-08-01 21:27:16 -0300

    fix styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lucas-oliveira-viana.github.io/",
-  base: "-my-portfolio",
   integrations: [tailwind()],
 });
